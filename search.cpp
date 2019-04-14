8613346e9d811ab86de780783df64efd2ae4e3cf
Andrey2102 <46657129+Andrey2102@users.noreply.github.com>
2019-04-14 23:22:48 +0300

    Add files via upload

---
+++ search.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+using namespace  std;
+
+struct Node
+{
+	Node* next;
+	int data;
+	bool empty = true;
+};
+
+typedef Node* PNode;
+
+class Hash{
+	PNode *Hash_Table;
+	int size;
+	public:
+		Hash() : Hash_Table(nullptr), size(0) {}
+		void generate_data(int);
+		void SIZE_inp();
+		int Hash_func(int);
+		void insert_elem(int);
+		void create_Hash_Table();
+		void output_HT();
+		PNode find(int);
+};
+
+PNode Hash::find(int key)
+{
+	Node* p;
+	p = Hash_Table[Hash_func(key)];
+	while (p && !(p->data == key))
+	{
+		p = p->next;
+	}
+	return p;
+}
+
+int Hash::Hash_func(int key){
+	return (key % size);
+}
+
+void Hash::insert_elem(int num)
+{
+	PNode p;
+	int h = Hash_func(num);
+	p = Hash_Table[h];
+	while(p && !p->empty)
+	{
+		p = p->next;
+	}
+	if (p->empty)
+	{
+	p->data = num;
+	p->empty = false;
+	}
+	else{
+		cout << endl << endl << "Hash table is overloaded.";
+	}
+}
+
+void Hash::generate_data(int count){
+	srand(time(NULL));
+	int n;
+	cout << endl << endl << "Generated numbers: " << endl;
+	for (int i = 0; i < count; ++i)
+	{
+		n = rand() % 100;
+		if (i % 10 == 0 && i > 0)
+		{
+			cout << endl;
+		}
+		cout << setw(4) << n;
+		insert_elem(n);
+	}
+}
+
+void Hash::SIZE_inp(){
+	cout << endl << endl << "Hash table size: ";
+	cin >> size;	
+}
+
+void Hash::output_HT(){
+	cout << endl << endl << "Hash table: " << endl;
+	for (int i = 0; i < size; ++i)
+	{
+		cout << i << " ";
+		Node *temp = Hash_Table[i];
+		while(temp){
+			cout << "-> " << temp->data;
+			temp = temp->next;		
+		}
+		cout << endl;
+	}
+}
+
+void Hash::create_Hash_Table()
+{
+	SIZE_inp();
+	Hash_Table = new Node*[size];
+	for (int i = 0; i < size; i++)
+	{
+		Hash_Table[i] = nullptr;
+	}
+	cout << endl << "Enter count of elements to insert in hash table: ";
+	int count_of_el;
+	cin >> count_of_el;
+	generate_data(count_of_el);
+	output_HT();
+}
+
+int main(){
+	Hash hash_t;
+	hash_t.create_Hash_Table();
+	cout << endl << endl << "Enter the number to search in the hash table: ";
+	int search;
+	cin >> search;
+	return 0;
+}
